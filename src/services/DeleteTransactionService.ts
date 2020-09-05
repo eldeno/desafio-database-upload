70d0a08aa1c5cd2553a1f8425bf7709a9b5eaead
Angelo <angelo.chagas@live.com>
2020-09-05 13:09:02 -0300

    chore: Routes config

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,21 @@
-// import AppError from '../errors/AppError';
+import { getRepository } from 'typeorm';
+
+import AppError from '../errors/AppError';
+import Transaction from '../models/Transaction';
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute(id: string): Promise<Transaction> {
+    const transactionsRepository = getRepository(Transaction);
+    const transaction = await transactionsRepository.findOne({
+      where: { id },
+    });
+    if (!transaction) {
+      throw new AppError('Transaction was not found', 404);
+    }
+
+    await transactionsRepository.remove(transaction);
+
+    return transaction;
   }
 }
 
